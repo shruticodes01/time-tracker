e2e97e905cfdd27e114ae57f888f679388c8cf8b
Shruti <dev.shruti27@gmail.com>
2025-06-10 16:32:22 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const dailyReport = document.querySelector("#daily-report");
+const weeklyReport = document.querySelector("#weekly-report");
+const monthlyReport = document.querySelector("#monthly-report");
+const cardReportGroups = document.querySelectorAll(".card__group");
+const highlightCardBackground = document.querySelectorAll(".card__text");
+
+console.log(cardReportGroups);
+console.log(highlightCardBackground);
